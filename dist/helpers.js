2915a116f89cf932c4150a190bbea6529b190bd8
Rafal F <functionalweasel@gmail.com>
2022-05-13 07:45:39 +0200

    feat: add in helpers respondWithErrors

--- dist/helpers.js
+++ dist/helpers.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.respondNotFound = void 0;
+exports.respondWithError = exports.respondNotFound = void 0;
 const respondNotFound = (res) => {
     res.status(404);
     res.send('Not found');
 };
 exports.respondNotFound = respondNotFound;
+const respondWithError = (res, error) => {
+    res.status(400);
+    res.json({ error });
+};
+exports.respondWithError = respondWithError;
--- src/helpers.ts
+++ src/helpers.ts
@@ -4,3 +4,8 @@ export const respondNotFound = (res: Response): void => {
   res.status(404);
   res.send('Not found') 
 }
+
+export const respondWithError = (res: Response, error: string): void => {
+  res.status(400);
+  res.json({error});
+}
